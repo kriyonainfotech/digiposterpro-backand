1ecbb16ed399330490b185ff92c31c0ca542427b
kriyonainfotech <kriyonainfotech@gmail.com>
2024-11-30 11:13:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 dotenv.config()
 ConnectDb()
 const corsOptions = {
-    origin: 'http://localhost:5173',  // Update with your client URL
+    origin: 'https://digiposterpro-admin-panel.vercel.app',  // Update with your client URL
     credentials: true, // This allows cookies to be sen 
 };
 app.use(cors(corsOptions));
